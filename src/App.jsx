75f07e212e085bb9bf3c647999d3b9392b3c03d1
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-04-14 23:17:50 +0600

    complete project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        
-      </div>
-      <h1>Vite + React</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       
-    </>
+    </div>
   )
 }
 
