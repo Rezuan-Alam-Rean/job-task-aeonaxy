8db03d1940c1202853b58449fb5e21e3735b521e
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-04-24 21:20:49 +0600

    remove unuse link

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -128,17 +128,7 @@ const Drawer = () => {
                                     <CgProfile />
                                     Add Blog
                                 </NavLink>
-                                <NavLink
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "flex px-4 gap-3 items-center bg-[#38d626] text-white p-2 rounded-xl  pr-4"
-                                            : "flex px-4 p-2 items-center mr-4 gap-3"
-                                    }
-                                    to="/dashboard/investments"
-                                >
-                                    <IoStatsChartSharp />
-                                    Investments
-                                </NavLink>
+                              
                             </ul>
                         </div>
                     </div>
